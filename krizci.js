c7415f75b2dc9af7ac5eda8650d26ffc9f333c08
chrispyYE <grm.kristjan@outlook.com>
2014-10-13 15:04:19 +0200

    asd
    asd

---
+++ krizci.js
@@ -0,0 +1,122 @@
+$(document).ready(function() {
+	var stevec=0;
+
+  $("#block1").click(function() {
+  	if(stevec%2==0){
+  		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click1");
+		}
+	else{
+		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click2");
+		}
+	});
+
+  $("#block2").click(function() {
+  	if(stevec%2==0){
+  		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click1");
+		}
+	else{
+		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click2");
+		}
+	});
+
+  $("#block3").click(function() {
+  	if(stevec%2==0){
+  		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click1");
+		}
+	else{
+		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click2");
+		}
+	});
+
+  $("#block4").click(function() {
+  	if(stevec%2==0){
+  		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click1");
+		}
+	else{
+		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click2");
+		}
+	});
+
+  $("#block5").click(function() {
+  	if(stevec%2==0){
+  		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click1");
+		}
+	else{
+		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click2");
+		}
+	});
+
+  $("#block6").click(function() {
+  	if(stevec%2==0){
+  		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click1");
+		}
+	else{
+		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click2");
+		}
+	});
+
+  $("#block7").click(function() {
+  	if(stevec%2==0){
+  		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click1");
+		}
+	else{
+		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click2");
+		}
+	});
+
+  $("#block8").click(function() {
+  	if(stevec%2==0){
+  		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click1");
+		}
+	else{
+		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click2");
+		}
+	});
+
+  $("#block9").click(function() {
+  	if(stevec%2==0){
+  		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click1");
+		}
+	else{
+		stevec++;
+  		$("#output").html(stevec);
+    	$(this).toggleClass("block_click2");
+		}
+	});
+
+
+});
