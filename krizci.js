b85c0858f6ccbfa1bb65d4ff7781cdcd96f4fde7
chrispyYE <grm.kristjan@outlook.com>
2014-10-13 16:39:00 +0200

    updatan izgled krozcev

--- krizci.js
+++ krizci.js
@@ -4,117 +4,117 @@ $(document).ready(function() {
   $("#block1").click(function() {
   	if(stevec%2==0){
   		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click1");
+      $(this).removeClass('block_click2')
+    	$(this).addClass("block_click1");
 		}
 	else{
 		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click2");
+      $(this).removeClass('block_click1')
+    	$(this).addClass("block_click2");
 		}
 	});
 
   $("#block2").click(function() {
   	if(stevec%2==0){
   		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click1");
+      $(this).removeClass('block_click2')
+    	$(this).addClass("block_click1");
 		}
 	else{
 		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click2");
+      $(this).removeClass('block_click1')
+    	$(this).addClass("block_click2");
 		}
 	});
 
   $("#block3").click(function() {
   	if(stevec%2==0){
   		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click1");
+      $(this).removeClass('block_click2')
+    	$(this).addClass("block_click1");
 		}
 	else{
 		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click2");
+      $(this).removeClass('block_click1')
+    	$(this).addClass("block_click2");
 		}
 	});
 
   $("#block4").click(function() {
   	if(stevec%2==0){
   		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click1");
+      $(this).removeClass('block_click2')
+    	$(this).addClass("block_click1");
 		}
 	else{
 		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click2");
+      $(this).removeClass('block_click1')
+    	$(this).addClass("block_click2");
 		}
 	});
 
   $("#block5").click(function() {
   	if(stevec%2==0){
   		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click1");
+      $(this).removeClass('block_click2')
+    	$(this).addClass("block_click1");
 		}
 	else{
 		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click2");
+      $(this).removeClass('block_click1')
+    	$(this).addClass("block_click2");
 		}
 	});
 
   $("#block6").click(function() {
   	if(stevec%2==0){
   		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click1");
+      $(this).removeClass('block_click2')
+    	$(this).addClass("block_click1");
 		}
 	else{
 		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click2");
+      $(this).removeClass('block_click1')
+    	$(this).addClass("block_click2");
 		}
 	});
 
   $("#block7").click(function() {
   	if(stevec%2==0){
   		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click1");
+      $(this).removeClass('block_click2')
+    	$(this).addClass("block_click1");
 		}
 	else{
 		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click2");
+      $(this).removeClass('block_click1')
+    	$(this).addClass("block_click2");
 		}
 	});
 
   $("#block8").click(function() {
   	if(stevec%2==0){
   		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click1");
+      $(this).removeClass('block_click2')
+    	$(this).addClass("block_click1");
 		}
 	else{
 		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click2");
+      $(this).removeClass('block_click1')
+    	$(this).addClass("block_click2");
 		}
 	});
 
   $("#block9").click(function() {
   	if(stevec%2==0){
   		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click1");
+      $(this).removeClass('block_click2')
+    	$(this).addClass("block_click1");
 		}
 	else{
 		stevec++;
-  		$("#output").html(stevec);
-    	$(this).toggleClass("block_click2");
+      $(this).removeClass('block_click1')
+    	$(this).addClass("block_click2");
 		}
 	});
 
