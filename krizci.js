76f0b70090cc8f7b477c055deac69c26c94094fe
chrispyYE <grm.kristjan@outlook.com>
2014-10-13 16:46:38 +0200

    sd
    sd

--- krizci.js
+++ krizci.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
 	var stevec=0;
 
+  $("#reset").click(function() {
+    var stevec=0;
+    $("#block1").removeClass('block_click2');
+    $("#block2").removeClass('block_click2');
+    $("#block2").removeClass('block_click1');
+    $("#block3").removeClass('block_click2');
+    $("#block3").removeClass('block_click1');
+    $("#block4").removeClass('block_click2');
+    $("#block4").removeClass('block_click1');
+    $("#block1").removeClass('block_click1');
+    $("#block5").removeClass('block_click2');
+    $("#block5").removeClass('block_click1');
+    $("#block6").removeClass('block_click2');
+    $("#block6").removeClass('block_click1');
+    $("#block7").removeClass('block_click2');
+    $("#block7").removeClass('block_click1');
+    $("#block8").removeClass('block_click2');
+    $("#block8").removeClass('block_click1');
+    $("#block9").removeClass('block_click2');
+    $("#block9").removeClass('block_click1');
+  });
+
   $("#block1").click(function() {
   	if(stevec%2==0){
   		stevec++;
